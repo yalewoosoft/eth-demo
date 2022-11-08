cdde8210e9798a35cda8311d7487a51dcf46accf
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-11-08 16:57:33 +0100

    emit events in contract

--- truffle/contracts/SocialNetwork.sol
+++ truffle/contracts/SocialNetwork.sol
@@ -4,10 +4,21 @@ contract SocialNetwork {
     mapping(bytes32 => string) public content;
     mapping(bytes32 => address payable) public sender;
 
+    event event_NewPost(
+        bytes32 post_hash,
+        string content,
+        address sender
+    );
+
+    event event_DeletePost(
+        bytes32 post_hash
+    );
+
     function new_post(string calldata post_content) public returns (bytes32) {
         bytes32 post_hash = keccak256(abi.encode(post_content));
         content[post_hash] = post_content;
         sender[post_hash] = payable(msg.sender);
+        emit event_NewPost(post_hash, post_content, msg.sender);
         return post_hash;
     }
 
@@ -16,6 +27,7 @@ contract SocialNetwork {
         require(sender[post_hash] == payable(msg.sender));
         content[post_hash] = "";
         sender[post_hash] = payable(0);
+        emit event_DeletePost(post_hash);
     }
 
     function get_post_by_hash(bytes32 post_hash) public view returns (string memory) {
