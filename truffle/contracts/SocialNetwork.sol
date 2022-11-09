70a5001ce5c5370d3ca45801879a528b1528bf9b
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-11-09 15:12:56 +0100

    implement backend upvote

--- truffle/contracts/SocialNetwork.sol
+++ truffle/contracts/SocialNetwork.sol
@@ -4,17 +4,24 @@ contract SocialNetwork {
     bytes32[] public posts;
     mapping(bytes32 => string) public content;
     mapping(bytes32 => address payable) public sender;
+    mapping(bytes32 => uint64) public upvotes;
 
     constructor() {
         posts.push(0xe746d9cf4809cb70939dde3a173485059e56117bd429912342bb47663d464ef7);
         content[0xe746d9cf4809cb70939dde3a173485059e56117bd429912342bb47663d464ef7] = "ABCDEFGHFSFKSFHKJHFS";
         sender[0xe746d9cf4809cb70939dde3a173485059e56117bd429912342bb47663d464ef7] = payable(0x433220a86126eFe2b8C98a723E73eBAd2D0CbaDc);
+        upvotes[0xe746d9cf4809cb70939dde3a173485059e56117bd429912342bb47663d464ef7] = 0;
     }
 
     event event_NewPost(
         bytes32 post_hash,
         string content,
         address sender
+    );
+
+    event event_Upvote(
+        bytes32 post_hash,
+        uint64 upvotes
     );
 
     /*
@@ -32,8 +39,16 @@ contract SocialNetwork {
         posts.push(post_hash);
         content[post_hash] = post_content;
         sender[post_hash] = payable(msg.sender);
+        upvotes[post_hash] = 0;
         emit event_NewPost(post_hash, post_content, msg.sender);
         return post_hash;
+    }
+
+    function upvote_post(bytes32 post_hash) public payable {
+        require(msg.value >= 1 gwei);
+        require(bytes(content[post_hash]).length > 0);
+        upvotes[post_hash] += 1;
+        emit event_Upvote(post_hash, upvotes[post_hash]);
     }
 
     /*
