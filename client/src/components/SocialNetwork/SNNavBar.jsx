aac5060d54823c269e7ac701cf788a04d76a1b06
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-11-15 14:37:25 +0100

    add current account to navbar

--- client/src/components/SocialNetwork/SNNavBar.jsx
+++ client/src/components/SocialNetwork/SNNavBar.jsx
@@ -2,8 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from "react-router-dom";
+import useEth from "../../contexts/EthContext/useEth";
+import {useEffect, useState} from "react";
 
 function SNNavBar() {
+    const { state: { accounts } } = useEth();
+    const [current_account, setCurrentAccount] = useState("");
+    useEffect(() => {
+        if (accounts) {
+            setCurrentAccount(accounts[0]);
+        }
+    }, [accounts])
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -16,6 +25,7 @@ function SNNavBar() {
                         <Nav.Link as={Link} to="/following">Manage Following</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
+                <Navbar.Text className="d-flex">{ current_account.substring(0, 8) }</Navbar.Text>
             </Container>
         </Navbar>
     )
